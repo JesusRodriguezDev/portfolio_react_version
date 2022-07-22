8d9f14bd095962c84c3f5abdbdbfcd77df79ac94
Jesus Rodriguez <jesusrodriguez@Jesuss-MacBook-Pro.local>
2022-07-22 14:37:46 -0600

    fixed styling issues in header component

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./header.css";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 
 const HeaderSocial = () => {
   return (
-    <div className="header__social">
-      <a href="https://linkedin.com" target="_blank">
+    <div className="header__socials">
+      <a className="btn" href="https://linkedin.com" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a className="btn" href="https://github.com" target="_blank">
         <BsGithub />
       </a>
     </div>
