eb2091cd1febc83b4e5a7d0989bd0dbf27b1b6a4
Jesus Rodriguez <jesusrodriguez@Jesuss-MacBook-Pro.local>
2022-07-22 00:34:57 -0600

    minor js and styling changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,9 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jesus Rodriguez</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">
+          Fullstack Developer / Data and Visualization Engineer
+        </h5>
         <CTA />
 
         <div className="me">
